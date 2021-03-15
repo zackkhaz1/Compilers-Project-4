f42926ed0d5051e3e1c361d91bfde6530ba1470a
zackkhaz1 <zack.khaz@gmail.com>
2021-03-14 22:46:41 -0500

    got started in these files

--- symbol_table.cpp
+++ symbol_table.cpp
@@ -8,11 +8,13 @@ ScopeTable::ScopeTable(){
 SymbolTable::SymbolTable(){
 	//TODO: implement the list of hashtables approach
 	// to building a symbol table:
-	// Upon entry to a scope a new scope table will be 
-	// entered into the front of the chain and upon exit the 
-	// latest scope table will be removed from the front of 
+	// Upon entry to a scope a new scope table will be
+	// entered into the front of the chain and upon exit the
+	// latest scope table will be removed from the front of
 	// the chain.
+	ScopeTable* st;
 	scopeTableChain = new std::list<ScopeTable *>();
+	scopeTableChain->push_front(st);
 }
 
 }
--- symbol_table.hpp
+++ symbol_table.hpp
@@ -14,18 +14,37 @@ using namespace std;
 namespace crona{
 
 //A semantic symbol, which represents a single
-// variable, function, etc. Semantic symbols 
-// exist for the lifetime of a scope in the 
-// symbol table. 
+// variable, function, etc. Semantic symbols
+// exist for the lifetime of a scope in the
+// symbol table.
+
 class SemSymbol {
-	//TODO add the fields that 
+	//TODO add the fields that
 	// each semantic symbol should track
 	// (i.e. the kind of the symbol (either a variable or function)
 	// and functions to get/set those fields
+public:
+	VarSym vs;
+	FnSym fs;
+
+
 };
 
-//A single scope. The symbol table is broken down into a 
-// chain of scope tables, and each scope table holds 
+class VarSym : public SemSymbol{
+public:
+		string type;
+		string name;
+
+};
+
+class FnSym : public SemSymbol{
+public:
+	 string type;
+	 string name;
+};
+
+//A single scope. The symbol table is broken down into a
+// chain of scope tables, and each scope table holds
 // semantic symbols for a single scope. For example,
 // the globals scope will be represented by a ScopeTable,
 // and the contents of each function can be represented by
@@ -37,6 +56,8 @@ class ScopeTable {
 		// and/or returning information to indicate
 		// that the symbol does not exist within the
 		// current scope.
+		auto findSymbol(string s) {return (symbols->find(s));}
+		HashMap<std::string, SemSymbol*>* getSymbols() {return (symbols);}
 	private:
 		HashMap<std::string, SemSymbol *> * symbols;
 };
@@ -46,12 +67,12 @@ class SymbolTable{
 		SymbolTable();
 		//TODO: add functions to create a new ScopeTable
 		// when a new scope is entered, drop a ScopeTable
-		// when a scope is exited, etc. 
+		// when a scope is exited, etc.
 	private:
 		std::list<ScopeTable *> * scopeTableChain;
 };
 
-	
+
 }
 
 #endif
